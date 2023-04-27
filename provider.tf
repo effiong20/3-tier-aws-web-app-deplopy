88d06f72ce1da21fa3b5f0d1556aed6a6daa9b52
effiong20 <120686961+effiong20@users.noreply.github.com>
2023-04-27 20:15:20 +0100

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,7 +1,7 @@
 provider "aws" {
    region     = "us-east-1"
-   access_key = "AKIA45KFE7XOQXWBID4B"
-   secret_key = "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"
+   access_key = ""
+   secret_key = ""
 }
 
 
